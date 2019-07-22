d876f4fdfc82ece7c34de8637e5d76780db77ead
root <root@hassio>
2019-07-22 23:21:54 +0200

    better bullets :)

--- static/js/node-red-contrib-deconz-helpers.js
+++ static/js/node-red-contrib-deconz-helpers.js
@@ -70,7 +70,7 @@ function deconz_getItemList(nodeItem, selectedItemElementName, options = {}) {
 
                             $.each(groupsByName, function(index, value) {
                                 if (value.meta.device_type == "groups") {
-                                    $('<option  value="group_' + value.meta.id +'">&bull;&nbsp' +value.meta.name +' (lights: '+value.meta.lights.length+')</option>').appendTo(groupHtml);
+                                    $('<option  value="group_' + value.meta.id +'">&#9675;&nbsp' +value.meta.name +' (lights: '+value.meta.lights.length+')</option>').appendTo(groupHtml);
                                 }
                             });
 
@@ -129,7 +129,7 @@ function deconz_getItemList(nodeItem, selectedItemElementName, options = {}) {
                             // $('<option value="' + value.topic + '"'+(selected ? 'selected' : '')+'>' + value.control_name + '</option>').appendTo(groupHtml);
                             //var name = (value.device_name).split(':',2);
                             var parentElement = (options.groups)?groupHtml:selectedItemElement;
-                            $('<option'+ disabled+' value="' + value.uniqueid +'">&bull;&nbsp' + value.device_name + (nameSuffix?' ('+nameSuffix+')':'') +'</option>').appendTo(parentElement);
+                            $('<option'+ disabled+' value="' + value.uniqueid +'">&#9679;&nbsp;' + value.device_name + (nameSuffix?' ('+nameSuffix+')':'') +'</option>').appendTo(parentElement);
                         });
 
                         // Enable item selection
