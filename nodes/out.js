a76cc26d9596d0d58590056ae98dd53b67c3007c
Andrey Popov <andrey_popov@me.com>
2019-07-22 23:13:36 +0300

    misc

--- nodes/out.js
+++ nodes/out.js
@@ -73,6 +73,22 @@ module.exports = function(RED) {
                                     payload = payload && payload != '0'?true:false;
                                     break;
 
+                                case 'toggle':
+                                    if ((/group_/g).test(node.config.device)) {
+                                        var groupid = ((node.config.device).split('group_').join(''));
+                                        var deviceMeta = node.server.getGroup(groupid);
+                                    } else {
+                                        var deviceMeta = node.server.getDevice(node.config.device);
+                                    }
+
+                                    // if (deviceMeta) {
+                                    //     payload = (node.config.state in node.meta.state)?node.meta.state[config.state]:node.meta.state
+                                    // } else {
+                                    //     payload = false;
+                                    // }
+                                    // payload = payload && payload != '0'?true:false;
+                                    break;
+
                                 case 'bri':
                                 case 'hue':
                                 case 'sat':
