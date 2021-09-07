16ce6d0a8c528d57520925c8d6a691e0e2474f8f
Zehir <zehir@zorim.fr>
2021-09-07 21:42:54 +0200

    Get node part 1

--- nodes/get.js
+++ nodes/get.js
@@ -1,103 +1,92 @@
+const ConfigMigration = require("../src/migration/ConfigMigration");
+const OutputMsgFormatter = require("../src/runtime/OutputMsgFormatter");
+
+
+const NodeType = 'deconz-get';
 module.exports = function (RED) {
     class deConzItemGet {
         constructor(config) {
             RED.nodes.createNode(this, config);
 
-            var node = this;
+            let node = this;
 
             node.config = config;
             node.cleanTimer = null;
 
             //get server node
             node.server = RED.nodes.getNode(node.config.server);
-            if (node.server) {
-                node.server.devices[node.id] = node.config.device; //register node in devices list
-
-                if (typeof (node.config.device) == 'string' && node.config.device.length) {
-                    var deviceMeta = node.server.getDevice(node.config.device);
-                    if (deviceMeta !== undefined && deviceMeta && "uniqueid" in deviceMeta) {
-                        node.server.devices[node.id] = deviceMeta.uniqueid; //register node in devices list
-                    } else {
-                        node.status({
-                            fill: "red",
-                            shape: "dot",
-                            text: "node-red-contrib-deconz/get:status.device_not_set"
-                        });
-                    }
-                } else {
-                    node.status({
-                        fill: "red",
-                        shape: "dot",
-                        text: "node-red-contrib-deconz/get:status.device_not_set"
-                    });
-                }
-            } else {
+            if (!node.server) {
                 node.status({
                     fill: "red",
                     shape: "dot",
                     text: "node-red-contrib-deconz/get:status.server_node_error"
                 });
+                return;
             }
 
-            if (typeof (config.device) == 'string' && config.device.length) {
-                node.status({}); //clean
-
-                node.on('input', function (message_in) {
-                    clearTimeout(node.cleanTimer);
-                    var deviceMeta = node.server.getDevice(node.config.device);
-
-                    if (deviceMeta) {
-                        node.server.devices[node.id] = deviceMeta.uniqueid;
-                        node.meta = deviceMeta;
-
-                        //status
-                        if ("state" in deviceMeta && deviceMeta.state !== undefined && "reachable" in deviceMeta.state && deviceMeta.state.reachable === false) {
-                            node.status({
-                                fill: "red",
-                                shape: "ring",
-                                text: "node-red-contrib-deconz/get:status.not_reachable"
-                            });
-                        } else if ("config" in deviceMeta && deviceMeta.config !== undefined && "reachable" in deviceMeta.config && deviceMeta.config.reachable === false) {
-                            node.status({
-                                fill: "red",
-                                shape: "ring",
-                                text: "node-red-contrib-deconz/get:status.not_reachable"
-                            });
-                        } else {
-                            node.status({
-                                fill: "green",
-                                shape: "dot",
-                                text: (config.state in node.meta.state) ? (node.meta.state[config.state]).toString() : "node-red-contrib-deconz/get:status.received",
-                            });
-
-                            node.send({
-                                payload: (config.state in node.meta.state) ? node.meta.state[config.state] : node.meta.state,
-                                payload_in: message_in.payload,
-                                meta: deviceMeta,
-                            });
-                        }
-
-                        node.cleanTimer = setTimeout(function () {
-                            node.status({}); //clean
-                        }, 3000);
-                    } else {
-                        node.status({
-                            fill: "red",
-                            shape: "dot",
-                            text: "node-red-contrib-deconz/get:status.device_not_set"
-                        });
-                    }
-
-                });
-            } else {
+            if (node.config.search_type === 'device' && node.config.device_list.length === 0) {
                 node.status({
                     fill: "red",
                     shape: "dot",
                     text: "node-red-contrib-deconz/get:status.device_not_set"
                 });
+                return;
             }
+
+            // Cleanup old status
+            node.status({});
+
+            node.on('input', function (message_in) {
+                clearTimeout(node.cleanTimer);
+                let msgs = new Array(this.config.output_rules.length);
+                let devices = [];
+
+                switch (node.config.search_type) {
+                    case 'device':
+                        for (let path of node.config.device_list) {
+                            devices.push({data: node.server.device_list.getDeviceByPath(path)});
+                        }
+                        break;
+                    case 'json':
+                    case 'jsonata':
+                        //TODO implement
+                        break;
+                }
+
+                node.config.output_rules.forEach((rule, index) => {
+                    // Only if it's not on start and the start msg are blocked
+
+                    // Clean up old msgs
+                    msgs.fill(undefined);
+
+                    // Format msgs, can get one or many msgs.
+                    let formatter = new OutputMsgFormatter(rule, NodeType, node.config);
+                    let msgToSend = formatter.getMsgs(devices, undefined, {src_msg: message_in});
+
+                    // Make sure that the result is an array
+                    if (!Array.isArray(msgToSend)) msgToSend = [msgToSend];
+
+                    // Send msgs
+                    for (let msg of msgToSend) {
+                        msgs[index] = msg;
+                        node.send(msgs);
+                    }
+                });
+
+                // TODO Display something usefull ?
+                node.status({
+                    fill: "green",
+                    shape: "dot",
+                    text: "node-red-contrib-deconz/get:status.received",
+                });
+
+                node.cleanTimer = setTimeout(function () {
+                    node.status({}); //clean
+                }, 3000);
+
+            });
         }
     }
 
-    RED.nodes.registerType('deconz-get', deConzItemGet);
+    RED.nodes.registerType(NodeType, deConzItemGet);
 };
