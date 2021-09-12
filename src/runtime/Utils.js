16d5038ebfdbd331d6f6ef409e9794b0496dfd68
Zehir <zehir@zorim.fr>
2021-09-12 15:24:17 +0200

    Add getNodeProperty util method

--- src/runtime/Utils.js
+++ src/runtime/Utils.js
@@ -1,7 +1,23 @@
+const REDUtil = require("@node-red/util/lib/util");
+
 class Utils {
     static sleep(ms) {
         return new Promise((resolve) => setTimeout(() => resolve(), ms));
     }
+
+
+    static getNodeProperty(property, node, message_in) {
+        if (property.type === 'jsonata') {
+            return REDUtil.evaluateJSONataExpression(
+                REDUtil.prepareJSONataExpression(property.value, node),
+                message_in,
+                undefined
+            );
+        } else if (['msg', 'flow', 'global'].includes(property.type)) {
+            return REDUtil.evaluateNodeProperty(property.value, property.type, node, message_in);
+        }
+    }
+
 }
 
 module.exports = Utils;
