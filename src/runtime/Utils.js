c0454f8227e3c62f7260d83b62f4b1506c3d17d3
Zehir <zehir@zorim.fr>
2021-09-13 22:18:44 +0200

    Cleanup getNodeProperty method by using the RED.utils method

--- src/runtime/Utils.js
+++ src/runtime/Utils.js
@@ -7,15 +7,7 @@ class Utils {
 
 
     static getNodeProperty(property, node, message_in) {
-        if (property.type === 'jsonata') {
-            return REDUtil.evaluateJSONataExpression(
-                REDUtil.prepareJSONataExpression(property.value, node),
-                message_in,
-                undefined
-            );
-        } else if (['msg', 'flow', 'global'].includes(property.type)) {
-            return REDUtil.evaluateNodeProperty(property.value, property.type, node, message_in);
-        }
+        return REDUtil.evaluateNodeProperty(property.value, property.type, node, message_in);
     }
 
 }
