6cc311040e1c2f2900e1b220f43f77a33ccccea0
Zehir <zehir@zorim.fr>
2021-09-14 22:06:01 +0200

    Add clone message utils method

--- src/runtime/Utils.js
+++ src/runtime/Utils.js
@@ -7,6 +7,19 @@ class Utils {
     }
 
 
+    static cloneMessage(message_in, moveData) {
+        if (moveData === undefined) moveData = [];
+        if (!Array.isArray(moveData)) moveData = [moveData];
+        let msg = REDUtil.cloneMessage(message_in);
+        for (const key of moveData) {
+            if (msg[key] !== undefined) {
+                msg[key + '_in'] = msg[key];
+                delete msg[key];
+            }
+        }
+        return msg;
+    }
+
     static getNodeProperty(property, node, message_in) {
         return REDUtil.evaluateNodeProperty(property.value, property.type, node, message_in);
     }
