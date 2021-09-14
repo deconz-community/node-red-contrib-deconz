11777a5ad896d9cfe08072ca82de8ac34c76646f
Zehir <zehir@zorim.fr>
2021-09-14 22:10:43 +0200

    Code formatting

--- src/runtime/Utils.js
+++ src/runtime/Utils.js
@@ -28,7 +28,7 @@ class Utils {
     static convertRange(value, r1, r2) {
         return Math.ceil((value - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0]);
     }
-    
+
 }
 
 module.exports = Utils;
