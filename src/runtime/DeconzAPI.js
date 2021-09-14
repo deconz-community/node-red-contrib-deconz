30179ea918a3b3af940d5c908498b8b752fc56e9
Zehir <zehir@zorim.fr>
2021-09-14 22:05:13 +0200

    Removed main url from each enpoint method

--- src/runtime/DeconzAPI.js
+++ src/runtime/DeconzAPI.js
@@ -19,7 +19,7 @@ class DeconzAPI {
             challenge: () => `${this.url.api()}/challenge`, // Undocumented
             main: () => `${this.url.api()}/${this.key}`,
             config: {
-                main: () => `${this.url.main()}/config`,
+                main: () => `/config`,
                 whitelist: (api_key) => `${this.url.config.main()}/whitelist${api_key !== undefined ? `/${api_key}` : ''}`,
                 update: () => `${this.url.config.main()}/update`,
                 updatefirmware: () => `${this.url.config.main()}/updatefirmware`,
@@ -39,14 +39,14 @@ class DeconzAPI {
                 wifiscan: () => `${this.url.config.main()}/wifiscan`, // Undocumented
             },
             capabilities: {
-                main: () => `${this.url.main()}/capabilities`,
+                main: () => `/capabilities`,
             },
             info: {
-                main: () => `${this.url.main()}/info`,
+                main: () => `/info`,
                 timezones: () => `${this.url.info.main()}/timezones`,
             },
             groups: {
-                main: (group_id) => `${this.url.main()}/groups${group_id !== undefined ? `/${group_id}` : ''}`,
+                main: (group_id) => `/groups${group_id !== undefined ? `/${group_id}` : ''}`,
                 action: (group_id) => `${this.url.groups.main(group_id)}/action`,
                 scenes: {
                     main: (group_id, scene_id) => `${this.url.groups.main(group_id)}/scenes${scene_id !== undefined ? `/${scene_id}` : ''}`,
@@ -61,38 +61,38 @@ class DeconzAPI {
                 }
             },
             lights: {
-                main: (light_id) => `${this.url.main()}/lights${light_id !== undefined ? `/${light_id}` : ''}`,
+                main: (light_id) => `/lights${light_id !== undefined ? `/${light_id}` : ''}`,
                 action: (light_id) => `${this.url.lights.main(light_id)}/state`,
                 groups: (light_id) => `${this.url.lights.main(light_id)}/groups`,
                 scenes: (light_id) => `${this.url.lights.main(light_id)}/scenes`,
                 connectivity: (light_id) => `${this.url.lights.main(light_id)}/connectivity` // Undocumented and can crash deconz
             },
             resourcelinks: {
-                main: (resourcelink_id) => `${this.url.main()}/resourcelinks${resourcelink_id !== undefined ? `/${resourcelink_id}` : ''}`
+                main: (resourcelink_id) => `/resourcelinks${resourcelink_id !== undefined ? `/${resourcelink_id}` : ''}`
             },
             rules: {
-                main: (rule_id) => `${this.url.main()}/rules${rule_id !== undefined ? `/${rule_id}` : ''}`
+                main: (rule_id) => `/rules${rule_id !== undefined ? `/${rule_id}` : ''}`
             },
             schedules: {
-                main: (schedule_id) => `${this.url.main()}/schedules${schedule_id !== undefined ? `/${schedule_id}` : ''}`
+                main: (schedule_id) => `/schedules${schedule_id !== undefined ? `/${schedule_id}` : ''}`
             },
             sensors: {
-                main: (sensor_id) => `${this.url.main()}/sensors${sensor_id !== undefined ? `/${sensor_id}` : ''}`,
+                main: (sensor_id) => `/sensors${sensor_id !== undefined ? `/${sensor_id}` : ''}`,
                 config: (sensor_id) => `${this.url.sensors.main(sensor_id)}/config`,
                 action: (sensor_id) => `${this.url.sensors.main(sensor_id)}/state`
             },
             touchlink: {
-                main: () => `${this.url.main()}/touchlink`,
+                main: () => `/touchlink`,
                 scan: () => `${this.url.touchlink.main()}/scan`,
                 identify: (result_id) => `${this.url.touchlink.main()}${result_id !== undefined ? `/${result_id}` : ''}/identify`,
                 reset: (result_id) => `${this.url.touchlink.main()}${result_id !== undefined ? `/${result_id}` : ''}/reset`
             },
             device: {
                 // Beta endpoint
-                main: (device_id) => `${this.url.main()}/devices${device_id !== undefined ? `/${device_id}` : ''}`,
+                main: (device_id) => `/devices${device_id !== undefined ? `/${device_id}` : ''}`,
             },
             userparameter: {
-                main: (userparameter_id) => `${this.url.main()}/userparameters${userparameter_id !== undefined ? `/${userparameter_id}` : ''}`,
+                main: (userparameter_id) => `/userparameters${userparameter_id !== undefined ? `/${userparameter_id}` : ''}`,
             }
         };
     }
