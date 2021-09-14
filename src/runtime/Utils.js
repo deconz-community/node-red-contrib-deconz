ff69cca37d0011522fdceecf258e80108ee1e1f4
Zehir <zehir@zorim.fr>
2021-09-14 22:08:11 +0200

    Move convertRange from DeconzHelper to Utils

--- src/runtime/Utils.js
+++ src/runtime/Utils.js
@@ -26,6 +26,10 @@ class Utils {
             REDUtil.evaluateNodeProperty(property.value, property.type, node, message_in);
     }
 
+    static convertRange(value, r1, r2) {
+        return Math.ceil((value - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0]);
+    }
+    
 }
 
 module.exports = Utils;
