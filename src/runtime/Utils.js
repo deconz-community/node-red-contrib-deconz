8f7a83bab92b0cf9fee5d4a3f229458a708e7d0e
Zehir <zehir@zorim.fr>
2021-09-29 21:45:03 +0200

    Check value type before calculating

--- src/runtime/Utils.js
+++ src/runtime/Utils.js
@@ -27,6 +27,7 @@ class Utils {
     }
 
     static convertRange(value, r1, r2) {
+        if (typeof value !== 'number') return;
         return Math.ceil((value - r1[0]) * (r2[1] - r2[0]) / (r1[1] - r1[0]) + r2[0]);
     }
 
