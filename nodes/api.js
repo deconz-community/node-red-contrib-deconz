24805eaba3d55b723a1324c334d2def75403f8c2
Zehir <zehir@zorim.fr>
2022-01-10 21:11:41 +0100

    Add Api Node Backend

--- nodes/api.js
+++ nodes/api.js
@@ -1,5 +1,6 @@
 const OutputMsgFormatter = require("../src/runtime/OutputMsgFormatter");
 const Utils = require("../src/runtime/Utils");
+const got = require("got");
 
 const NodeType = 'deconz-api';
 module.exports = function (RED) {
@@ -30,6 +31,7 @@ module.exports = function (RED) {
             node.config = config;
             node.ready = false;
 
+            node.cleanStatusTimer = null;
             node.status({});
 
             //get server node
@@ -62,6 +64,79 @@ module.exports = function (RED) {
             } else {
                 node.server.on('onStart', initNode);
             }
+
+            node.on('input', (message_in, send, done) => {
+                // For maximum backwards compatibility, check that send and done exists.
+                send = send || function () {
+                    node.send.apply(node, arguments);
+                };
+                done = done || function (err) {
+                    if (err) node.error(err, message_in);
+                };
+                (async () => {
+                    let waitResult = await Utils.waitForEverythingReady(node);
+                    if (waitResult) {
+                        done(RED._(waitResult));
+                        return;
+                    }
+
+                    // Load the config
+                    let config = node.config;
+                    let methods = ['GET', 'POST', 'PUT', 'DELETE'];
+                    let method = Utils.getNodeProperty(config.specific.method, node, message_in, methods);
+                    // Make sure the method is valid
+                    if (!methods.includes(method)) method = 'GET';
+                    let endpoint = Utils.getNodeProperty(config.specific.endpoint, node, message_in);
+                    let payload = Utils.getNodeProperty(config.specific.payload, node, message_in);
+
+                    // Add pending status
+                    node.status({
+                        fill: "blue",
+                        shape: "dot",
+                        text: "node-red-contrib-deconz/get:status.pending"
+                    });
+
+                    // Do request
+                    const response = await node.server.api.doRequest(endpoint, {method, body: payload});
+
+                    // Add output properties
+                    let outputProperties = {
+                        payload: response.body,
+                        status: {
+                            code: response.statusCode,
+                            message: response.statusMessage,
+                        }
+                    };
+                    let outputMsg = Utils.cloneMessage(message_in, Object.keys(outputProperties));
+                    Object.assign(outputMsg, outputProperties);
+                    outputMsg.topic = config.topic;
+
+                    // Clear status timer
+                    if (node.cleanStatusTimer) {
+                        clearTimeout(node.cleanStatusTimer);
+                        node.cleanStatusTimer = null;
+                    }
+
+                    // Set status
+                    node.status({
+                        fill: response.statusCode === 200 ? "green" : "red",
+                        shape: "dot",
+                        text: `${response.statusCode}: ${response.statusMessage}`
+                    });
+
+                    // Add status cleanup timer
+                    node.cleanStatusTimer = setTimeout(function () {
+                        node.status({}); //clean
+                    }, 3000);
+
+                    // Send the message
+                    send(outputMsg);
+                    done();
+
+                })().then().catch((error) => {
+                    console.error(error);
+                });
+            });
 
             node.on('close', (removed, done) => {
                 done();
